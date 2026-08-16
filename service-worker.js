8403abec228037e11ad4710976f268f8aeb9c173
Alejandro <alejalingar@gmail.com>
2026-08-16 12:42:29 +0200

    Añadir aviso de progresión de peso por ejercicio

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* Al cambiar el contenido de la app, sube este número:
    es lo que hace saltar el aviso de "hay una versión nueva". */
-const CACHE_VERSION = 'plan-ejercicio-v4';
+const CACHE_VERSION = 'plan-ejercicio-v5';
 
 const APP_SHELL = [
   './',
